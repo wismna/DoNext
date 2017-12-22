6dce3a6262e53cea48ce2fb411825a784330615c
BONNEVILLE Geoffroy <gbonneville@mega.com>
2017-12-22 11:36:33 +0100

    Adds back button on History activity

--- app/src/main/java/com/wismna/geoffroy/donext/activities/HistoryActivity.java
+++ app/src/main/java/com/wismna/geoffroy/donext/activities/HistoryActivity.java
@@ -1,6 +1,7 @@
 package com.wismna.geoffroy.donext.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.wismna.geoffroy.donext.R;
@@ -15,5 +16,11 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        ActionBar toolbar = getSupportActionBar();
+
+        // Show back button on toolbar
+        assert toolbar != null;
+        toolbar.setDisplayHomeAsUpEnabled(true);
+        toolbar.setDisplayShowHomeEnabled(true);
     }
 }
